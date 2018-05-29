5efe272e9b96c8cfbc482b90cc9dff02d9b5cc41
upat <up4.3at@gmail.com>
2018-05-29 21:07:36 +0900

    月計算に対応, font-displayプロパティでchromeにおける動作の改善

--- js/age_cal.js
+++ js/age_cal.js
@@ -26,6 +26,7 @@ const start_cera = 0;
 try{
 	// コード実行時の日時を取得
 	var now_date = new Date();
+	now_date.setDate(1);
 }catch(e){
 	// 取得できなかった場合は2018年1月1日をセット
 	console.log(e);
@@ -58,10 +59,7 @@ d3_html.append('input')
 d3_html.append('span').text('は');
 
 // 誕生年セレクトボックスの設置
-d3_html.append('select')
-	.attrs({
-		id       : 'age_year_box'
-	});
+d3_html.append('select').attrs({ id : 'age_year_box' });
 // 誕生年セレクトボックスの中身
 let age_box_loop = 0;
 for(age_box_loop = goal_cera;age_box_loop>=start_cera;age_box_loop--){
@@ -85,10 +83,7 @@ for(age_box_loop = goal_cera;age_box_loop>=start_cera;age_box_loop--){
 d3_html.append('span').text('年');
 
 // 誕生月セレクトボックスの設置
-d3_html.append('select')
-	.attrs({
-		id       : 'age_month_box'
-	});
+d3_html.append('select').attrs({ id : 'age_month_box' });
 // 誕生月セレクトボックスの中身
 age_box_loop = 0; // 2回目
 for(age_box_loop = 0;age_box_loop<month;age_box_loop++){
@@ -103,9 +98,6 @@ d3_html.append('span').text('月生まれの、');
 
 // 改行
 d3_html.append('br');
-
-// テキスト
-d3_html.append('span').style('margin-left', '10px').text('満');
 
 // 年齢テキストボックスの設置
 d3_html.append('input')
@@ -132,11 +124,11 @@ d3_html.append('input')
 		onclick : 'age_calculation()'
 	});
 
-// メイン処理
+// 年月日計算を行うメイン処理
 function age_calculation(){
 	// テキスト出力領域のクリア
 	d3.select('#result_txt').remove();
-		
+	
 	// 名前の取得(無い場合はデフォルト値を入れる)
 	const name = document.getElementById('name_txt');
 	if(!name.value){ name.value = '私'; }
@@ -145,29 +137,36 @@ function age_calculation(){
 	if(!year.value){ year.value = 2001; }
 	// 誕生月の取得(無い場合はデフォルト値を入れる)
 	const month = document.getElementById('age_month_box');
-	if(!month.value){ month.value = 1; }
+	if(!month.value){ month.value = 0; }
 	// 年齢の取得(無い場合はデフォルト値を入れる)
 	const age = document.getElementById('age_txt');
-	if(!age.value){ age.value = 17; }
+	if(!age.value){ age.value = default_age; }
 	
 	// 入力値を入れる
 	const input_date = new Date(year.value, month.value, 1);
 	
-	console.log(input_date.getMonth() + ', ' + now_date.getMonth());
-	console.log();
-	console.log(now_date.getMonth() - input_date.getMonth());
+	// 実年齢
+	let actual_age = now_date.getFullYear() - input_date.getFullYear();
 	
-	// 実年齢の計算
-	const diff_year = now_date.getFullYear() - year.value;
+	// 経過月の取得
+	let aa_month = new Date(); // 月計算のために使用
+	aa_month.setDate(1);       // 1日に揃える(日付までは考慮しない)
+	
+	input_date.setFullYear(now_date.getFullYear()); // 月の比較のため、年を合わせる
+	if(now_date < input_date){                      // 月の比較
+		actual_age--;                               // 年内でも誕生月を跨いでいない時
+	}
+	aa_month.setMonth(now_date.getMonth() - input_date.getMonth()); // 月の差を計算
+	
 	// 出力テキスト
 	let result_txt;
-	
-	if(diff_year == age.value){
+	if(actual_age == age.value){
 		result_txt = name.value + 'は正直者です。';
-	}else if(diff_year > age.value){
-		result_txt = name.value + 'は満' + age.value + '歳' + (diff_year-age.value)*12 + 'ヶ月です。';
+	}else if(actual_age > age.value){
+		const fake_month = (actual_age - age.value) * 12 + aa_month.getMonth();
+		result_txt = name.value + 'は' + age.value + '歳' + fake_month + 'ヶ月です。';
 	}else{
-		result_txt = name.value + 'は' + (age.value-diff_year) + '年後からやってきた。○ル・○サイ・コン○ルゥ';
+		result_txt = name.value + 'は' + (age.value-actual_age) + '年後からやってきた。○ル・○サイ・コン○ルゥ';
 	}
 	
 	// テキスト出力領域の設置
@@ -179,12 +178,12 @@ function age_calculation(){
 	d3.select('#result_txt').append('div').attr('id', 'tweet_button');
 	try{
 		twttr.widgets.createShareButton(
-			'https://upat.github.io/age_cal.html',  // URL(非表示の場合は空白スペースを入れること)
+			'https://upat.github.io/age_cal.html', // URL(非表示の場合は空白スペースを入れること)
 			document.getElementById('tweet_button'),
 			{
-				size     : 'large',    // ツイートボタンの大きさ
-				text     : result_txt,  // ツイート本文
-				hashtags : '年齢詐称じぇねれーた'
+				size     : 'large',               // ツイートボタンの大きさ
+				text     : result_txt,            // ツイート本文
+				hashtags : '年齢詐称じぇねれーた' // ハッシュタグ
 			}
 		);
 	}catch(e){
@@ -194,7 +193,7 @@ function age_calculation(){
 
 // 年齢テキストボックスの監視、常に半角数字以外を除去
 function reject_char(){
-	// 年齢テキストボックスの要素
+	// 年齢テキストボックスの要素を取得
 	const input_str = document.getElementById('age_txt');
 	
 	// 半角数字以外を排除・配列化->文字列へ戻す
@@ -204,8 +203,7 @@ function reject_char(){
 	num = Number(num);
 	num = String(num);
 	
-	console.log(num.length);
-	input_str.value = num.length > 4 ? num.slice(0,4) : num; // 5桁以上の時、4桁まで削る
+	input_str.value = num.length > 4 ? num.slice(0, 4) : num; // 5桁以上の時、4桁まで削る
 }
 
 // input要素へ入力された文字に対してサロゲートペアを考慮した文字数カウントを行い、トリミングする
